ebc5aa041ad38765bc9edf6607042da03d9e27ab
a9ek0 <109074282+a9ek0@users.noreply.github.com>
2023-02-15 19:15:17 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,7 @@
 #include <stdio.h>
-struct qwe
+
+int main (void)
 {
-    char name[20];
-    int weight;
-    float trunk_size;
-};
-void arr_structure_print(struct qwe structure[], int num_of_elements);
-void arr_struct_create(struct qwe structure[], int num_of_elements);
-
-int main() {
-    int x = 2;
-    struct qwe no[x];
-            //[5] = {{}, {}, {}, {}, {}};
-    struct qwe *da = &no;
-    arr_struct_create(da, x);
-    arr_structure_print(da, x);
-    return 0;
-}
-
-void arr_structure_print(struct qwe structure[], int num_of_elements)
-{
-    for (int i = 0; i < num_of_elements; ++i)
-    {
-        printf("\nN is: %s", structure[i].name);
-        printf("\nW is: %d", structure[i].weight);
-        printf("\nT is: %1.3f", structure[i].trunk_size);
-    }
-}
-
-
-void arr_struct_create(struct qwe structure[], int num_of_elements)
-{
-    for (int i = 0; i < num_of_elements; ++i)
-    {
-        printf("\nEnter n\n");
-        scanf_s("%s", structure[i].name);
-        fflush(stdin);
-        printf("\nEnter w\n");
-        scanf_s("%d", &structure[i].weight);
-        printf("\nEnter t\n");
-        scanf_s("%f", &structure[i].trunk_size);
-    }
-
+  puts ("Hello, World!");
+  return 0;
 }
